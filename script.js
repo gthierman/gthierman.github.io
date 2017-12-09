13b2eb04eecfdb20ef9644115e7ab2a65d464499
gthierman <geoff.thierman@clio.com>
2017-12-09 15:30:37 -0800

    Deleted unneeded files and styled the content div

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(function(){
-  var left = $('.content .wrapper').offset().left;
-  var top = $('.content .wrapper').offset().top;
-  console.log(left);
-})
+// $(function(){
+//   var left = $('.content .wrapper').offset().left;
+//   var top = $('.content .wrapper').offset().top;
+//   console.log(left);
+// })
